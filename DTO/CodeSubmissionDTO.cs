ef09277d4554adf035d520e863aab86cb5560f6d
José Ferrari <licoverpa321@gmail.com>
2025-01-29 15:02:29 -0300

    removing field.

--- DTO/CodeSubmissionDTO.cs
+++ DTO/CodeSubmissionDTO.cs
@@ -3,7 +3,7 @@
     public class CodeSubmissionDTO
     {
         public string Code { get; set; } // The actual code snippet being submitted.
-        public string Language { get; set; } // Optionally, the programming language (C#, Java, etc.).
+        // public string Language { get; set; } // Optionally, the programming language (C#, Java, etc.).
 
         // You can add other fields as necessary, such as:
         // public string UserId { get; set; } // Optional user info for tracking who submitted the code.
