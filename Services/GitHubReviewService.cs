c73f55993bafddb29e7ec931ccfcf20318b7f61d
José Ferrari <licoverpa321@gmail.com>
2025-02-17 21:27:24 -0300

    Adding Service for Comment feature

---
+++ Services/GitHubReviewService.cs
@@ -0,0 +1,22 @@
+﻿using CodeReviewAssistant.DTO;
+using OpenAI.Chat;
+
+namespace CodeReviewAssistant.Services
+{
+    public interface IGitHubReviewService
+    {
+        void PostReviewAsComment(string comment);
+    }
+
+    public class GitHubReviewService : IGitHubReviewService
+    {
+        public GitHubReviewService()
+        {            
+        }
+
+        public void PostReviewAsComment(string comment)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
